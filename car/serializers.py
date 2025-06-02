b4d43658a9cd66cf59d63983c0658dd089eadaa5
Miwacer <il>
2025-06-02 21:11:44 +0300

    fix

--- car/serializers.py
+++ car/serializers.py
@@ -1,26 +1,19 @@
 from rest_framework import serializers
 from .models import Car
 
-
 class CarSerializer(serializers.Serializer):
-    manufacturer = serializers.CharField(
-        max_length=64
-    )
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(
-        min_value=50,
-        max_value=1500
-    )
+    horse_powers = serializers.IntegerField(min_value=50, max_value=1500)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(
-        allow_null=True,
-        required=False
-    )
+    problem_description = serializers.CharField(allow_null=True, required=False)
 
     def create(self, validated_data):
-        return Car(**validated_data)
+        return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
+        instance.save()
         return instance
--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import json
 from car.models import Car
 from car.serializers import CarSerializer
 
-
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    json_str = json.dumps(serializer.data)
+    json_str = json.dumps(serializer.data, separators=(",", ":"))
     return json_str.encode()
-
 
 def deserialize_car_object(json_bytes: bytes) -> Car:
     json_str = json_bytes.decode()
