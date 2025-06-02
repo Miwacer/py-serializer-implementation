adb7e7ddd501f7ab077d0dbf9679daff3e63e87f
Miwacer <il>
2025-06-02 21:06:17 +0300

    fix flake8

--- car/serializers.py
+++ car/serializers.py
@@ -3,11 +3,19 @@ from .models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    manufacturer = serializers.CharField(max_length=64)
+    manufacturer = serializers.CharField(
+        max_length=64
+    )
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(min_value=50, max_value=1500)
+    horse_powers = serializers.IntegerField(
+        min_value=50,
+        max_value=1500
+    )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True, required=False)
+    problem_description = serializers.CharField(
+        allow_null=True,
+        required=False
+    )
 
     def create(self, validated_data):
         return Car(**validated_data)
