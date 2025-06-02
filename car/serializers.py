b01f560316a506d74e7b116457fb6cc3a18101c0
Miwacer <il>
2025-06-02 21:14:04 +0300

    fix flake8

--- car/serializers.py
+++ car/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Car
 
+
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(
