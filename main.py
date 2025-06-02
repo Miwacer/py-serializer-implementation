5c83b97af9dd2a9b9d4391ebcd48492d42293045
Miwacer <il>
2025-06-02 21:13:12 +0300

    fix flake8

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import json
 from car.models import Car
 from car.serializers import CarSerializer
 
+
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     json_str = json.dumps(serializer.data, separators=(",", ":"))
     return json_str.encode()
+
 
 def deserialize_car_object(json_bytes: bytes) -> Car:
     json_str = json_bytes.decode()
